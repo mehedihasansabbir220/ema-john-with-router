04475281674817f686d7e617c77364d5e7c76e0e
mohammadmehedihasansabbir <86853457+mohammadmehedihasansabbir@users.noreply.github.com>
2021-09-30 23:07:23 +0600

    first change

---
+++ src/components/Inventory/Inventory.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Inventory = () => {
+    return (
+        <div>
+            <h2>I am inventory </h2>
+            <h3>this is my invantion </h3>
+        </div>
+    );
+};
+
+export default Inventory;
